adcf866b5c8be74dcc687fc7e814e3298723e98b
jamesreynold-cloud <james.reynold@pursuit.org>
2026-01-28 19:20:40 -0500

    Simplify Vercel config to serve static files properly

--- api/feedback.js
+++ api/feedback.js
@@ -34,13 +34,10 @@ function initializeData() {
 
 export default function handler(req, res) {
   // Enable CORS
-  res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+  res.setHeader('Content-Type', 'application/json');
 
   // Handle OPTIONS request
   if (req.method === 'OPTIONS') {
